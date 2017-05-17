95d11f1c6d100dccc3a7618ff0f061260edcc743
Rob Ungar <robungar@gmail.com>
2017-05-16 23:26:43 -0400

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require('webpack')
+var path = require('path')
 
 module.exports = {
 	entry: {
@@ -7,14 +7,27 @@ module.exports = {
 	},
 	output: {
 		filename: 'public/build/bundle.js',
-		sourceMapFilename: 'public/build/bundle.map'
+    sourceMapFilename: 'public/build/bundle.map'
 	},
-  devtool: '#source-map',	
+    devtool: '#source-map',	
+	plugins: process.env.NODE_ENV === 'production' ? [
+	    new webpack.DefinePlugin({
+        'process.env': {
+        	'NODE_ENV': JSON.stringify('production')
+        }
+	    }),
+    	new webpack.optimize.UglifyJsPlugin({
+    		minimize: true,
+		    compress: {
+		        warnings: true
+		    }
+    	})
+	] : [],	
 	module: {
 		loaders: [
 			{
 				test: /\.jsx?$/,
-				exclude: /(node_modules)/,
+				exclude: /(node_modules|bower_components)/,
 				loader: 'babel-loader',
 				query:{
 					presets:['react', 'es2015']
@@ -23,3 +36,4 @@ module.exports = {
 		]
 	}
 }
+
